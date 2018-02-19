139c0d8027045ef6e6ddc09065a38f3674a7535c
Spas <Vutovspas@gmail.com>
2018-02-19 20:48:45 +0200

    contract in progress

--- contracts/SingleBet.sol
+++ contracts/SingleBet.sol
@@ -10,12 +10,31 @@ contract SingleBet {
     string private homeTeamName;
     string private awayTeamName;
 
+    uint8 private choiceHome = 1;
+    uint8 private choiceAway = 2;
+    uint8 private choiceDraw = 3;
+
+    /**
+    * @dev Mapping of placed bets per choice
+    * choice 1 = home team win
+    * choice 2 = away team win
+    * choice 3 = draw
+    * @param choice => address => amount
+    */
+    mapping (uint8=>mapping (address=>uint)) bets;
+
     modifier isSettled() {
         require(isOpen == false);
         _;
     }
 
+    modifier isNotOver() {
+        require(isOpen == true);
+        _;
+    }
+
     event Settle(uint8 wnr);
+    event Bet(uint8 choice, address addr, uint256 amount);
 
     function SingleBet(address _oracle, string _homeTeamName, string _awayTeamName) public {
         owner = msg.sender;
@@ -23,6 +42,7 @@ contract SingleBet {
         homeTeamName = _homeTeamName;
         awayTeamName = _awayTeamName;
         isOpen = true;
+        // TODO time close bets?
     }
 
     function getBetMetaInfo() public view returns(bool, string, string) {
@@ -33,6 +53,10 @@ contract SingleBet {
         return winner;
     }
 
+    /**
+    * @dev Oracle settles the bet.
+    * @param _winner 1 - home team, 2 - away team, 3 - draw
+    */
     function settleBet(uint8 _winner) public {
         require(msg.sender == oracle);
         Settle(_winner);
@@ -40,5 +64,53 @@ contract SingleBet {
         winner = _winner;
 
         // TODO payout
+    }
+
+    /**
+    * @dev Place bet on open event.
+    * @param choice 1 - home team, 2 - away team, 3 - draw
+    */
+    function bet(uint8 choice) isNotOver public payable {
+        require(choice == choiceHome || choice == choiceAway || choice == choiceDraw);
+        Bet(choice, msg.sender, msg.value);
+        // todo require for overflow + tests
+    }
+
+    /**
+    * @dev Get bets placed by Address
+    * @param addr Betting address
+    * @return (home team bet amount, away team bet amount, draw bet amount)
+    */
+    function getBets(address addr) public view returns(uint, uint, uint) {
+        return (bets[choiceDraw][addr], bets[choiceDraw][addr], bets[choiceDraw][addr]);
+    }
+
+    function checkTotalBalance() public view returns(uint) {
+       return this.balance; 
+    }
+
+    // TODO
+    function getPossibleWinnig() public pure returns(uint) {
+        return 0;
+    }
+
+    // TODO
+    function manualGetWinnigs() public pure {
+
+    }
+
+    // TODO
+    function manualSettleBet() public pure {
+
+    }
+
+    // TODO
+    function changeOracle() public pure {
+        
+    }
+
+    // TODO
+    function cancleEvent() public pure {
+
     }
 }
--- test/TestSingleBet.js
+++ test/TestSingleBet.js
@@ -30,6 +30,42 @@ contract('SingleBet', function (accounts) {
       });
     });
 
+    describe("settleBet", () => {
+      it("should return error when NOT Oracle", async function () {
+        // arrange
+
+        // act
+        try {
+          await instance.settleBet(2, {
+            from: accounts[3]
+          });
+        } catch (ex) {
+          assert.isNotNull(ex);
+        }
+
+        // assert
+        assert.ok(true);
+      });
+
+      it("should set as settled when Oracle", async function () {
+        // arrange
+        let winner = 1;
+
+        // act       
+        await instance.settleBet(winner, {
+          from: initialParams._oracle
+        });
+        let data = await instance.getBetMetaInfo();
+
+        // assert
+        assert.equal(false, data[0], "Bet is open");
+      });
+    });
+
+    it("should payout when Oracle", async function () {
+      assert.fail();
+    });
+
     describe("getWinner", () => {
       it("should return error when NOT settled", async function () {
         // arrange
@@ -51,7 +87,7 @@ contract('SingleBet', function (accounts) {
         await instance.settleBet(winner, {
           from: initialParams._oracle
         });
-        
+
         // act
         let result = await instance.getWinner();
 
@@ -62,41 +98,128 @@ contract('SingleBet', function (accounts) {
         assert.equal(winner, result)
       });
     });
+
+    describe("bet", () => {
+      it("should NOT be allowed when settled", async function () {
+        // arrange
+        await instance.settleBet(1, {
+          from: initialParams._oracle
+        });
+
+        // act
+        try {
+          await instance.bet(1, {
+            from: accounts[4],
+            value: web3.toWei(2, 'ether')
+          })
+        } catch (ex) {
+          assert.isNotNull(ex);
+        }
+
+        // assert
+        assert.ok(true);
+      });
+
+      it("should accept bet when open choice 1", async function () {
+        // arrange
+        let betAmount = web3.toWei(2, 'ether');
+        let acc = accounts[4];
+        // act
+        await instance.bet(1, {
+          from: acc,
+          value: betAmount
+        })
+
+        // assert 
+        let money = await instance.checkTotalBalance();
+        console.log(betAmount);
+        console.log(money);
+        let placedBets = await instance.getBets(acc);
+        console.log(placedBets);
+        assert.fail();
+      });
+
+      it("should accept bet when open choice 2", async function () {
+        // arrange
+
+        // act
+
+        // assert 
+        assert.fail();
+      });
+
+      it("should accept bet when open multiple bets", async function () {
+        // arrange
+
+        // act
+
+        // assert 
+        assert.fail();
+      });
+
+      it("should return error when invalid choice", async function () {
+        // arrange
+
+        // act
+        try {
+          await instance.isNotOver(4);
+        } catch (ex) {
+          assert.isNotNull(ex);
+        }
+
+        // assert 
+        assert.ok(true);
+      });
+    });
+    describe("getPossibleWinnig", () => {
+      it("should get possible winning", async function () {
+        // arrange
+
+        // act
+
+        // assert
+        assert.fail();
+      });
+    });
+    describe("manualGetWinnigs", () => {
+      it("should TODO", async function () {
+        // arrange
+
+        // act
+
+        // assert
+        assert.fail();
+      });
+    });
+    describe("manualSettleBet", () => {
+      it("should TODO", async function () {
+        // arrange
+
+        // act
+
+        // assert
+        assert.fail();
+      });
+    });
+    describe("changeOracle", () => {
+      it("should TODO", async function () {
+        // arrange
+
+        // act
+
+        // assert
+        assert.fail();
+      });
+    });
+    describe("cancleEvent", () => {
+      it("should TODO", async function () {
+        // arrange
+
+        // act
+
+        // assert
+        assert.fail();
+      });
+    });
   });
-
-  // it("should send coin correctly", function() {
-  //   var meta;
-
-  //   // Get initial balances of first and second account.
-  //   var account_one = accounts[0];
-  //   var account_two = accounts[1];
-
-  //   var account_one_starting_balance;
-  //   var account_two_starting_balance;
-  //   var account_one_ending_balance;
-  //   var account_two_ending_balance;
-
-  //   var amount = 10;
-
-  //   return MetaCoin.deployed().then(function(instance) {
-  //     meta = instance;
-  //     return meta.getBalance.call(account_one);
-  //   }).then(function(balance) {
-  //     account_one_starting_balance = balance.toNumber();
-  //     return meta.getBalance.call(account_two);
-  //   }).then(function(balance) {
-  //     account_two_starting_balance = balance.toNumber();
-  //     return meta.sendCoin(account_two, amount, {from: account_one});
-  //   }).then(function() {
-  //     return meta.getBalance.call(account_one);
-  //   }).then(function(balance) {
-  //     account_one_ending_balance = balance.toNumber();
-  //     return meta.getBalance.call(account_two);
-  //   }).then(function(balance) {
-  //     account_two_ending_balance = balance.toNumber();
-
-  //     assert.equal(account_one_ending_balance, account_one_starting_balance - amount, "Amount wasn't correctly taken from the sender");
-  //     assert.equal(account_two_ending_balance, account_two_starting_balance + amount, "Amount wasn't correctly sent to the receiver");
-  //   });
-  // });
 });
